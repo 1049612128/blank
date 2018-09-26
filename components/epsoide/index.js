2d384b061c3ac32ccd8ba21de41c4c91733d4cde
1049612128 <1049612128@qq.com>
2018-09-26 16:19:26 +0800

    add like,movie,epsoide

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -4,7 +4,17 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    index:Number
+    index:{
+      type:Number,
+      observer: function (newVal, oldVal, changedPath) {
+        let val = newVal < 10 ? "0" + newVal : newVal
+        console.log(val)
+        this.setData({
+          _index:val
+        })
+      }
+    }
+    
   },
 
   /**
@@ -12,9 +22,20 @@ Component({
    */
   data: {
     month:"七月",
-    year:2018
+    year:2018,
+    _index:"",
+    months:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"]
   },
-
+  attached:function(){
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth();
+    // console.log(month)
+    this.setData({
+      year: year,
+      month: this.data.months[month]
+    })
+  },
   /**
    * 组件的方法列表
    */
